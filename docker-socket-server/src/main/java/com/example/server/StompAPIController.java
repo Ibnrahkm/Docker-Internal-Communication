4801557fd29162ae210f0ad89d277c0ac0698128
ibnrahkm <mdibrahimk935@gmail.com>
2024-04-30 16:24:19 +0600

    kubernetes minikube added

--- docker-socket-server/src/main/java/com/example/server/StompAPIController.java
+++ docker-socket-server/src/main/java/com/example/server/StompAPIController.java
@@ -2,6 +2,8 @@ package com.example.server;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
@@ -12,5 +14,11 @@ public class StompAPIController {
     @SendTo("/topic/greetings")
     public String greeting(String message) {
         return ("Hello, " + message + "!");
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/check")
+    public String getPodName() {
+        return System.getenv("HOSTNAME");
     }
 }
